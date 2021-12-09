a2d29230e796738a8a5a566e3722151a15e98f78
Alex Groat <alex.groat.95@gmail.com>
2021-12-09 12:38:05 +1030

    Port files and setup server.js

---
+++ server..js
@@ -0,0 +1,45 @@
+// import required packages and modules
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers')
+
+const sequelize = require('./config/connection');
+
+// create a new sequelize store using express-session
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers });
+
+// configure and link a session object with the sequelize store
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+
+// add express-session and store middleware
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
