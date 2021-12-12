4aeb018c26062836c12a681ecbab85001b672a6a
Alex Groat <alex.groat.95@gmail.com>
2021-12-13 00:44:14 +1030

    get all posts

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -6,6 +6,32 @@ const {
 } = require('../../models');
 
 // get all posts
+router.get("/", (req, res) => {
+    Post.findAll({
+            attributes: ["id", "content", "title", "created_at"],
+            order: [
+                ["created_at", "DESC"]
+            ],
+            include: [{
+                    model: User,
+                    attributes: ["username"],
+                },
+                {
+                    model: Comment,
+                    attributes: ["id", "comment_content", "post_id", "user_id", "created_at"],
+                    include: {
+                        model: User,
+                        attributes: ["username"],
+                    },
+                },
+            ],
+        })
+        .then((dbPostData) => res.json(dbPostData))
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 // get one post
 
