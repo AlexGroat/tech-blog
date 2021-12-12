871a66556d9d240b2f81e98e475e14f577c75861
Alex Groat <alex.groat.95@gmail.com>
2021-12-12 23:16:18 +1030

    Model index comp

--- models/index.js
+++ models/index.js
@@ -8,10 +8,34 @@ User.hasMany(Post, {
     foreignKey: 'user_id'
 });
 
-// Each post belongs to a user
+// A user can comment on a post
+User.hasMany(Comment, {
+    foreignKey: 'user_id'
+});
 
+// Each post belongs to a user
 Post.belongsTo(User, {
     foreignKey: 'user_id',
     onDelete: "cascade"
 });
 
+// Each post can contain multiple comments
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+});
+
+// Each comment belongs to a user
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// Each comment is tied to a post
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+module.exports = {
+    User,
+    Post,
+    Comment
+};
