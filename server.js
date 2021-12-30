ab6bb9fe87ba68a2e92bd3fcf2653ca4e175573c
Alex Groat <alex.groat.95@gmail.com>
2021-12-30 12:59:04 +1030

    Server.js

--- server.js
+++ server.js
@@ -1,45 +1,43 @@
-// import required packages and modules
 const path = require('path');
 const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers')
-
 const sequelize = require('./config/connection');
-
-// create a new sequelize store using express-session
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({
+    helpers
+});
+const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'Superbalize',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+        checkExpirationInterval: 1000 * 60 * 10, 
+        expiration: 1000 * 60 * 30 
+    })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({ helpers });
-
-// configure and link a session object with the sequelize store
-const sess = {
-  secret: "Superbalize",
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
-
-
-// add express-session and store middleware
-app.use(session(sess));
-
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+sequelize.sync();
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
 });
